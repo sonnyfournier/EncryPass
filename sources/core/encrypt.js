8f77629894467d35eb345cb2d1217731c0b19846
R3J3CT3D <R3J3CT3D@protonmail.com>
2017-11-16 14:16:30 +0100

    [~] Correct and change encryption with possibility to use the old gen

--- sources/core/encrypt.js
+++ sources/core/encrypt.js
@@ -8,29 +8,32 @@
 */
 
 // Comments are coming soon
-function 	encryptPassword(form) {
+function encryptPassword(form) {
 
-    var 	res      = 0;
-    var 	newPass  = "";
-    var 	service  = form.serviceInput.value;
-    var 	username = form.usernameInput.value;
-    var 	password = form.passwordInput.value;
+  var res = 0;
+  var newPass = "";
+  var service = form.serviceInput.value;
+  var username = form.usernameInput.value;
+  var password = form.passwordInput.value;
 
-    if (service) {
-        if (username) {
-            service += username;
-        }
-        service = service.toLowerCase();
-        for (var i = 0; i < service.length; i++) {
-            res += service.charAt(i).charCodeAt(0);
-        }
-        if (password) {
-            for (var i = 0; i < password.length; i++) {
-                var tmp = ('!'.charCodeAt(0) + ((password.charAt(i).charCodeAt(0) - '!'.charCodeAt(0)) + res) % 94);
-                newPass += String.fromCharCode(tmp)
-            }
-            form.newInput.value = newPass;
-        }
+  if (service) {
+    if (username && username != "OLD") {
+      service += username;
     }
-    return (false);
+    service = service.toLowerCase();
+    for (var i = 0; i < service.length; i++) {
+      res += service.charAt(i).charCodeAt(0);
+    }
+    if (password) {
+      for (var i = 0; i < password.length; i++) {
+        if (username == "OLD")
+          var tmp = ('!'.charCodeAt(0) + ((password.charAt(i).charCodeAt(0) - '!'.charCodeAt(0)) + res) % 94);
+        else
+          var tmp = ('!'.charCodeAt(0) + ((password.charAt(i).charCodeAt(0) - '!'.charCodeAt(0)) + res + i) % 94);
+        newPass += String.fromCharCode(tmp)
+      }
+      form.newInput.value = newPass;
+    }
+  }
+  return (false);
 }
